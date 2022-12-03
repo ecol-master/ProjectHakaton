505f985a0315757c9277353e71e77dfe07d414d0
YolkinEgor <yolkinegor@gmail.com>
2022-12-03 21:32:01 +0500

    Фикс авторизации (Входа в аккаунт). Фикс бага с сохранением пароля в модели.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,9 +134,9 @@ STATIC_URL = 'static/'
 CORS_ORIGIN_ALLOW_ALL = True
 
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:8000'
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
